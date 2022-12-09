b982e655426cbfc21d8c9da561c0c1daced310bb
Ilnur Garrapov <garrapovilnur@gmail.com>
2022-12-09 19:08:00 +0300

    15th ex

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ string input = Console.ReadLine();
 Console.WriteLine("{0}->{1}",input, input[1]);
 */
 
+/*
 int ThirdDigit(int number)
 {
     int result = -1;    
@@ -25,3 +26,33 @@ if (ThirdDigit(number1) == -1)
 Console.WriteLine("Третьей цифры нет");
 else
 Console.WriteLine($"Третья цифра: {ThirdDigit(number1)}");
+*/
+
+int dayNumber = ReadInt("Введите номер дня от 1 до 7: ");
+Console.WriteLine(WorkHoliday(dayNumber));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string WorkHoliday(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write( + a + " день - Выходной");
+        }
+        else
+        {
+            Console.Write( + a + " день - Рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("Это число не соответствует значению от 1 до 7!");
+    }
+    return " день.";
+}
