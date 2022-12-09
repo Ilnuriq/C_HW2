d4b6c70318dad876d6bb09916198b144d9748797
Ilnur Garrapov <garrapovilnur@gmail.com>
2022-12-09 19:02:06 +0300

    13th ex

--- Program.cs
+++ Program.cs
@@ -1,4 +1,27 @@
-﻿
+﻿/*
 Console.WriteLine("Введите трехзначное число:");
 string input = Console.ReadLine();
 Console.WriteLine("{0}->{1}",input, input[1]);
+*/
+
+int ThirdDigit(int number)
+{
+    int result = -1;    
+    if (number >=100)
+    {
+        while (number > 999)
+    {
+        number = number / 10;
+    }
+    result = number % 10;
+    }
+return result; 
+}
+
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (ThirdDigit(number1) == -1)
+Console.WriteLine("Третьей цифры нет");
+else
+Console.WriteLine($"Третья цифра: {ThirdDigit(number1)}");
